4f9fca2f86fb624476b01487ca0a9b0904f72616
jbc909090 <78177014+jbc909090@users.noreply.github.com>
2021-02-13 13:24:52 -0600

    Update SupplierList.java

--- SupplierList.java
+++ SupplierList.java
@@ -62,4 +62,18 @@ public class SupplierList implements Serializable {
     public String toString() {
         return suppliers.toString();
     }
+    
+    //adding a function for going through the linked list finding a specfic ID
+    //takes a int, returns a int (both are to be supplier IDs)
+    public int IDcheck (int ID) {
+        for (int i=0; i < suppliers.size(); i++) {
+            Supplier temp = suppliers.get(i);
+            if (ID == temp.get_ID()) {
+                System.out.println("ID found");
+                return i;
+            }
+        }
+        System.out.println("ERROR: ID is not in database");
+        return -1;
+    }
 }
